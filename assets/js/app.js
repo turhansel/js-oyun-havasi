efe1906f98903aedb06e761ee1cd54fb00c8a427
Turhan Sel <turhansel@Turhan-MacBook-Pro.local>
2021-03-12 12:01:03 +0300

    challenge-3 added.

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,9 +16,91 @@ function reset() {
 
 // Challenge2: Cat Generator
 
-function generateCat(){
+function generateCat() {
     var image = document.createElement('img');
     var div = document.getElementById('flex-cat-gen');
-    image.src="https://thecatapi.com/api/images/get?format=src&type=gif&size=small"
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small"
     div.appendChild(image);
+}
+
+// Challenge3: Rock, Paper, Scissors
+
+function rpsGame(yourChoice) {
+    // console.log(yourChoice)
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('Computer Choice:', botChoice);
+
+    results = decideWinner(humanChoice, botChoice);
+    console.log(results);
+
+    message = finalMessage(results);
+    console.log(message);
+
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    // JSON
+    var rpsDatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        'scissors': { 'paper': 1, 'scissors': 0.5, 'rock': 0 },
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return { 'message': '🤪 LOSER 🤪', 'color': 'red' };
+    }
+    else if (yourScore === 0.5) {
+        return { 'message': 'Almost 🧐', 'color': 'yellow' };
+    }
+    else {
+        return { 'message': 'You Won!', 'color': 'green' };
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+
+    //let's remove all the images
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow:0px 10px 50px rgba(37, 50, 233, 1); '>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + ";font-size:48px; padding:20px;'>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow:0px 10px 50px rgba(243, 38, 24, 1);'>"
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+
+
+
+
 }
